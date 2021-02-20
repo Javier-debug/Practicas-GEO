a47494c7dd3320b2305bce7f700c86ec73f2bb37
Javier-debug <madrajavi@gmail.com>
2021-02-19 18:51:21 -0600

    practica 1 v2

--- mapa.js
+++ mapa.js
@@ -2,8 +2,8 @@ AOS.init();
 
 function iniciaMapa() {
   var coordenadas = {
-    lat: 21.152639,
-    lng: -101.711598
+    lat: 21.14785,
+    lng: -101.70345
   }
 
   var map = new google.maps.Map(document.getElementById('mapa'), {
